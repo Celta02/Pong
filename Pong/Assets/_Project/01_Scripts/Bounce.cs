5222c1543d7c2d98d4453c05a66e23220dfca7cf
Celta02 <r.espinoza.tagle@gmail.com>
2021-12-30 00:35:01 -0400

    Update Bounce.cs
    tooltip for the X Bounce Factor.

--- Pong/Assets/_Project/01_Scripts/Bounce.cs
+++ Pong/Assets/_Project/01_Scripts/Bounce.cs
@@ -4,7 +4,8 @@ namespace CeltaGames._Project._01_Scripts
 {
     public class Bounce : MonoBehaviour
     {
-        [SerializeField] float _balanceFactor = 1.5f;
+        [Tooltip("This factor multiplies the X direction after Bounce. Allowing to have less occurrences of the ball hitting form barrier to barrier.")]
+        [SerializeField] float _xBounceFactor = 1.15f;
         Rigidbody _rigidbody;
         BallSpeed _speed;
 
@@ -18,7 +19,7 @@ namespace CeltaGames._Project._01_Scripts
         {
             var normal = collision.contacts[0].normal;
             var newDir = Vector3.Reflect(_rigidbody.velocity, normal).normalized;
-            newDir += new Vector3(newDir.x*_balanceFactor, 0f, 0f);
+            newDir += new Vector3(newDir.x * _xBounceFactor, 0f, 0f);
             _rigidbody.velocity = newDir.normalized * _speed.CurrentSpeed;
             
         }
